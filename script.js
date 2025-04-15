cff09e54792ed5e1b179091ef4dd41034366bafa
sunfr0st <118242244+andreybglol@users.noreply.github.com>
2025-04-15 20:41:51 +0300

    added files for homepage and added planet pages
    i added homepage and made this project for my cousin to try create something for fun don't judge :D

---
+++ script.js
@@ -0,0 +1,23 @@
+function createStars() {
+  const container = document.querySelector("body");
+  for (let i = 0; i < 1000; i++) {
+    // Increase the number of stars to 1000
+    const star = document.createElement("div");
+    star.className = "star";
+    star.style.width = ".1px";
+    star.style.height = ".1px";
+    star.style.top = Math.random() * 100 + "%";
+    star.style.left = Math.random() * 100 + "%";
+    container.appendChild(star);
+  }
+}
+createStars();
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+};
